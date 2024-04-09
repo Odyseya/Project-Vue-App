5e2068d961475dd04e187a7b095d887c53ad8a59
Anna <ania.pwk.ap@gmail.com>
2024-04-09 23:22:30 +0200

    task list adjust

--- src/views/TaskListView.vue
+++ src/views/TaskListView.vue
@@ -1,36 +1,39 @@
-<!-- example of taskList, not applied anywhere -->
-
 <template>
   <h3>Tasks</h3>
+  <!-- task -->
   <ul id="list" class="list">
     <li
       v-for="task in tasks"
       :key="task.id"
       :class="task.value === true ? 'completed' : 'incompleted'"
     >
-      {{ task.title }} - {{ task.isDone ? 'Done' : 'Not Done' }}
-      ><button class="delete-btn" @click="deleteTask(task.id)">x</button>
+      {{ task.title }} - {{ task.isDone ? 'Done' : 'Not Done' }} ><button
+        class="delete-btn"
+        @click="deleteTask(task.id)"
+      >
+        x
+      </button>
     </li>
   </ul>
 </template>
 
 <script>
-import { fetchAllTasks } from '@/api/tasksApi';
+import { fetchAllTasks } from '@/api/tasksApi'
 
 export default {
- data() {
+  data() {
     return {
-      tasks: [],
-    };
- },
- async created() {
-    try {
-      const fetchedTasks = await fetchAllTasks();
-      console.log('Fetched tasks:', fetchedTasks);
-      this.tasks = fetchedTasks;
-    } catch (error) {
-      console.error('Failed to fetch tasks:', error);
+      tasks: []
     }
- },
-};
+  },
+  async created() {
+    try {
+      const fetchedTasks = await fetchAllTasks()
+      console.log('Fetched tasks:', fetchedTasks)
+      this.tasks = fetchedTasks
+    } catch (error) {
+      console.error('Failed to fetch tasks:', error)
+    }
+  }
+}
 </script>
