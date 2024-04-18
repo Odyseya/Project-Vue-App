8bbfff9b24375d7b15c822428cb1126858868668
Anna <ania.pwk.ap@gmail.com>
2024-04-18 18:52:12 +0200

    adjust TaskItem styles

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -45,48 +45,46 @@ const toggleTaskCompletion = async () => {
 </script>
 
 <template>
-  <li class="bg-[#3490dc52]">
-    <!-- EDIT TITLE  -->
-    <div v-if="!editMode">
-      {{ task.title }}
-    </div>
-    <input
-      v-else
-      type="text"
-      v-model="newTitle"
-      class="border rounded px-2 py-1 w-full md:w-auto"
-    />
+  <div class="flex">
+    <li class="items-start w-full bg-[#3490dc52] rounded-md shadow-lg p-4">
+      <!-- <li class="items-start w-full bg-white rounded-md shadow-lg p-4"> -->
+      <!-- EDIT TITLE  -->
+      <div v-if="!editMode">
+        {{ task.title }}
+      </div>
+      <input
+        v-else
+        type="text"
+        v-model="newTitle"
+        class="text-gray-500 border rounded px-2 py-1 w-full md:w-auto mr-4"
+      />
 
-    <!-- CHECKBOX TO TOGGLE TASK COMPLETION -->
-    <!-- Unique ID for each checkbox to prevent toggling the first checkbo clicking on any "completed" label-->
-    <input
-      type="checkbox"
-      id="completionCheckbox"
-      :id="`completionCheckbox-${task.id}`"
-      v-model="task.is_complete"
-      @change="toggleTaskCompletion"
-    />
-    <label :for="`completionCheckbox-${task.id}`"> Completed </label>
+      <!-- CHECKBOX TO TOGGLE TASK COMPLETION -->
+      <!-- Unique ID for each checkbox to prevent toggling the first checkbo clicking on any "completed" label-->
+      <input
+        type="checkbox"
+        id="completionCheckbox"
+        :id="`completionCheckbox-${task.id}`"
+        v-model="task.is_complete"
+        @change="toggleTaskCompletion"
+      />
+      <label :for="`completionCheckbox-${task.id}`"> Completed </label>
 
-    <!-- EDIT & DELETE BUTTONS -->
-    <button @click="_editTask" class="btn edit-btn">{{ editMode ? 'Save' : 'Edit' }}</button>
-    <button @click="_deleteTask" class="btn delete-btn">Delete</button>
-  </li>
+      <!-- EDIT & DELETE BUTTONS -->
+      <button @click="_editTask" class="btn edit-btn">{{ editMode ? 'Save' : 'Edit' }}</button>
+      <button @click="_deleteTask" class="btn delete-btn">Delete</button>
+    </li>
+  </div>
 </template>
 
 <style scoped>
+input[type='checkbox'] {
+  margin-right: 5px;
+}
 li {
   border-radius: 10px;
-  margin-bottom: 10px;
+  margin-top: 10px;
   padding: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-input[type='checkbox'] {
-  margin-right: 10px;
-}
-
-.btn {
-  margin-left: 5px;
 }
 </style>
