312db3f66b5b64632035104ca3247e787b1b51f2
Anna <ania.pwk.ap@gmail.com>
2024-04-25 21:32:45 +0200

    404 page styles

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <h1>404 - Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-    <router-link to="/">Go back to Home</router-link>
+  <div class="max-w-screen-md mx-auto px-4 py-10">
+    <h1 class="text-4xl font-bold text-center mb-4">404 - Page Not Found</h1>
+    <p class="text-lg text-center mb-4">The page you are looking for does not exist.</p>
+    <router-link
+      to="/"
+      class="text-center block w-full max-w-xs mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Go back to Home
+    </router-link>
   </div>
 </template>
 
-<!-- to setup -->
-<script>
-export default {
-  name: 'NotFoundView'
-}
-</script>
+<script setup></script>
 
 <style scoped></style>
