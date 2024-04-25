eab4b4f00742e77e74f29c14a4da84a87b2dc850
Anna <ania.pwk.ap@gmail.com>
2024-04-25 20:46:27 +0200

    adjust form size styles to match registration

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -54,56 +54,54 @@ const signIn = async () => {
 </script>
 
 <template>
-  <main>
-    <div class="max-w-screen-sm mx-auto px-4 py-10">
-      <form @submit.prevent="signIn" class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
-        <h1 class="text-3xl text-at-light-green mb-4">Login to your Account</h1>
+  <div class="max-w-screen-md px-4 py-10 mx-auto">
+    <form @submit.prevent="signIn" class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+      <h1 class="text-3xl text-at-light-green mb-4">Login to your Account</h1>
 
-        <div class="flex flex-col mb-2">
-          <label for="email" class="mb-1 text-sm text-at-light-green">User email: </label>
-          <input
-            type="text"
-            required
-            id="email"
-            v-model="email"
-            class="p-2 text-gray-500 focus:outline-none"
-          />
-        </div>
+      <div class="flex flex-col mb-2">
+        <label for="email" class="mb-1 text-sm text-at-light-green">User email: </label>
+        <input
+          type="text"
+          required
+          id="email"
+          v-model="email"
+          class="p-2 text-gray-500 focus:outline-none"
+        />
+      </div>
 
-        <div class="flex flex-col mb-2">
-          <label for="password" class="mb-1 text-sm text-at-light-green">Password:</label>
-          <input
-            type="password"
-            required
-            id="password"
-            v-model="password"
-            class="p-2 text-gray-500 focus:outline-none"
-          />
-        </div>
+      <div class="flex flex-col mb-2">
+        <label for="password" class="mb-1 text-sm text-at-light-green">Password:</label>
+        <input
+          type="password"
+          required
+          id="password"
+          v-model="password"
+          class="p-2 text-gray-500 focus:outline-none"
+        />
+      </div>
 
-        <!-- Display the error message from store-->
-        <div v-if="userStore.errorMessage" class="text-red-500">
-          {{ userStore.errorMessage }}
-        </div>
-        <!-- Display the error message from this component-->
-        <!-- <div v-if="errorMsg" class="text-red-500">
+      <!-- Display the error message from this component-->
+      <!-- <div v-if="errorMsg" class="text-red-500">
       {{ errorMsg }}
     </div> -->
-        <button
-          type="submit"
-          class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
-        >
-          Login
-        </button>
+      <!-- Display the error message from store-->
+      <div v-if="userStore.errorMessage" class="text-red-500 mx-auto">
+        {{ userStore.errorMessage }}
+      </div>
+      <button
+        type="submit"
+        class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+      >
+        Login
+      </button>
 
-        <p class="text-sm mt-6 text-center">
-          <RouterLink class="text-sm mt-6 text-center text-at-light-green" to="/register">
-            Don't have an account? <span class="text-at-light-green">Register</span>
-          </RouterLink>
-        </p>
-      </form>
-    </div>
-  </main>
+      <p class="text-sm mt-6 text-center">
+        <RouterLink class="text-sm mt-6 text-center text-at-light-green" to="/register">
+          Don't have an account? <span class="text-at-light-green">Register</span>
+        </RouterLink>
+      </p>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
